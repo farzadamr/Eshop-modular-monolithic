bec8ee24e57a868b3ff9fdcc2f37c7e19f6835d2
Farzad <amirifarzad1381@gmail.com>
2025-03-16 16:58:30 +0330

    swagger configuration

--- EndPoint/Api/Program.cs
+++ EndPoint/Api/Program.cs
@@ -1,6 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
+}
+
+app.MapGet("/Health", () => "Hello World! The System is running");
 
 app.Run();
