570fb2f179173b351cef980505b4259b3c0fa7f6
Farzad <amirifarzad1381@gmail.com>
2025-03-16 17:29:57 +0330

    wire up dependency injection for each module

--- EndPoint/Api/Program.cs
+++ EndPoint/Api/Program.cs
@@ -1,21 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
-
+﻿using Basket;
+using Catalog;
+using Ordering;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Add Services to container!
+builder.Services
+    .AddBasketModule(builder.Configuration)
+    .AddCatalogModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
+
+
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-
-}
-
-app.MapGet("/Health", () => "Hello World! The System is running");
 
 app.Run();
---
+++ Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket;
+public static class BasketModule
+{
+    public static IServiceCollection AddBasketModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        //Add Basket Module services to container!
+
+
+        return services;
+    }
+}
